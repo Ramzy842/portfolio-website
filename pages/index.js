239ba80bdadc5ab6930f5b0dcfd77538bf1374fc
Ramzy842 <ramzychahbani@gmail.com>
2022-08-16 15:06:10 +0100

    fix 1.0

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,16 @@ import Projects from "../components/Work/Projects";
 import tech from "../data/tech";
 import projects from "../data/projects";
 import Experience from "../components/Experience/Experience";
-import jobs from "../data/Jobs";
+import jobs from "../data/jobs";
 import Contact from "../components/Contact";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Image from "next/image"
-import "swiper/css";
+
 export default function Home({ technologies, projects, jobs }) {
   return (
     <div>
       <Head></Head>
       <Hero technologies={technologies} />
       <Projects projects={projects} />
-      <Experience technologies={technologies} jobs={jobs} />
-      
+      <Experience technologies={technologies} jobs={jobs} /> 
       <Contact />
     </div>
   );
